1d36b140d5318ce38b280c0b8a3f5f64d86777b3
Meliodas-R <inceptiongodmaxwell@gmail.com>
2021-11-02 10:27:34 +0100

    Agregada la función de conversión binaria y la muestra del resultado por pantalla.

--- Ejercicio_14_sharp/Program.cs
+++ Ejercicio_14_sharp/Program.cs
@@ -6,7 +6,14 @@ namespace Ejercicio_14_sharp
     {
         static void Main(string[] args)
         {
+            Console.Write("Introduzca un número: ");
+            string numero = Console.ReadLine();
             
+            int baseNumero = 10;
+            int baseConversion = 2;
+
+            string resultado = Convert.ToString(Convert.ToInt32(numero, baseNumero), baseConversion);
+            Console.WriteLine("\nEl número " + numero + " en binario es: " + resultado);
         }
     }
 }
